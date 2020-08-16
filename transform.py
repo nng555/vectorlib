b75244d2b11f98fb9defdad2bc15ff4fa5432347
annabelng <a.h.ng952@gmail.com>
2020-08-15 17:43:34 -0700

    tested forward

--- transform.py
+++ transform.py
@@ -66,6 +66,7 @@ class LinearTransform:
         returns - transformed vector
         """
 
+        # transform x vector
         #initilizae vals with rows=rows of transform and columns=columns of v
         vals = [[(0) for i in range(v.shape[1])]for x in range(len(self.transform))]
 
@@ -88,22 +89,56 @@ class LinearTransform:
         else:
             raise Exception('not matching dimensions')
 
+        '''
+        # vector x transform
+        #initilizae vals with rows=rows of v and columns=columns of transform
+
+        if(len(v.shape)==1):
+            v.shape.insert(0,1)
+
+        vals = [[(0) for i in range(self.out_dim)]for x in range(v.shape[0])]
+
+        if (v.shape[1] == self.in_dim):
+
+            #rows of first matrix
+            for i in range(v.shape[0]):
+                #columns of second matrix
+                for j in range(self.out_dim):
+                    #rows of second matrix
+                    for k in range(self.in_dim):
+                        vals[i][j] += v.values[i][k] * self.transform[k][j]
+
+            transV = Vector(vals,None)
+            return transV
+
+        else:
+            raise Exception('not matching dimensions')'''
+
 class LinearTransformTest(unittest.TestCase):
 
     def setUp(self):
-        self.v1 = Vector([[1], [3], [2]])
+        self.v1 = Vector([1, 3, 2])
         self.t1 = LinearTransform(
-                transform=[[1, 2, 3],
-                           [4, 5, 6]])
+                transform=[[1, 2],
+                            [3,4],
+                            [5,6]])
+        self.v2 = Vector([[1], [3], [2]])
+        self.t2 = LinearTransform(
+                transform=[[1, 2,3],
+                            [4,5,6]])
+
         print(self.t1)
-        print(self.v1.shape)
+        print(self.t1.transform)
+        print(self.v1.values)
         self.random = LinearTransform.random_init(4,2)
         print(self.random)
 
     def testTransform(self):
         tmp = Vector([[13], [31]])
-        print ((self.t1.forward(self.v1)).values)
-        self.assertEqual(tmp.values, (self.t1.forward(self.v1)).values)
+        print ((self.t2.forward(self.v2)).values)
+
+        #print ((self.t1.forward(self.v1)).values)
+        self.assertEqual(tmp.values, (self.t2.forward(self.v2)).values)
 
 if __name__ == "__main__":
     unittest.main()
